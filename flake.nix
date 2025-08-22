e60c4670ed255b8c31b56d1715bc99f49590d52c
Tom Aylott <oblivious@subtlegradient.com>
2025-08-22 16:01:51 -0400

    Refactor: Use mkShellNoCC for devShell
    Switches to `mkShellNoCC` in `flake.nix` for the development shell. This change removes the C compiler requirement for the shell, making it
    more lightweight and suitable for projects that do not need compilation.

--- flake.nix
+++ flake.nix
@@ -1,30 +1,27 @@
+# effect-native/flake.nix
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
-  outputs =
-    { nixpkgs, ... }:
-    let
-      forAllSystems =
-        function:
-        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
-          system: function nixpkgs.legacyPackages.${system}
-        );
-    in
-    {
-      formatter = forAllSystems (pkgs: pkgs.alejandra);
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            bun
-            corepack
-            deno
-            maestro
-            nodejs-slim_23
-            python3
-            yq-go
-          ];
-        };
-      });
-    };
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+        system: function nixpkgs.legacyPackages.${system}
+      );
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          bun
+          corepack
+          deno
+          maestro
+          nodejs-slim_23
+          python3
+          yq-go
+        ];
+      };
+    });
+  };
 }
