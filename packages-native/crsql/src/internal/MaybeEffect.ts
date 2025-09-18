6cb84a54d2276fdc8d0164c51bf3020581f6110c
Tom Aylott <oblivious@subtlegradient.com>
2025-09-18 16:46:46 -0400

    Refactor: Add missing documentation comments for MaybeEffect type and function

--- packages-native/crsql/src/internal/MaybeEffect.ts
+++ packages-native/crsql/src/internal/MaybeEffect.ts
@@ -1,10 +1,16 @@
-/** @internal */
+/**
+ * @since 0.3.0
+ * @internal */
 import * as Effect from "effect/Effect"
 
-/** @internal */
+/**
+ * @since 0.3.0
+ * @internal */
 export type MaybeEffect<A, E = never, R = never> = Effect.Effect<A, E, R> | A
 
-/** @internal */
+/**
+ * @since 0.3.0
+ * @internal */
 export function MaybeEffect(self: null | undefined): null
 export function MaybeEffect<A, E = never, R = never>(self: MaybeEffect<A, E, R>): Effect.Effect<A, E, R>
 export function MaybeEffect<A, E = never, R = never>(self: MaybeEffect<A, E, R> | null | undefined) {
